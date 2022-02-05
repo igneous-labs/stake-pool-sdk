0bd80158126b6ea16798e6b954da236a3168533b
Jesse Y. Cho <jesse@socean.fi>
2022-02-04 20:15:52 -0800

    add boilerplate cli with get-stakepool-account command

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,30 @@
 #!/usr/bin/env node
-import { foo } from './main'
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
 
-foo()
+import { Socean } from './main';
+
+const SOCEAN = new Socean();
+
+const getStakePoolAccount = async (argv) => {
+    if (argv.verbose) {
+        console.info('fetching Socean stake pool account info');
+    }
+    console.log(await SOCEAN.getStakePoolAccount());
+};
+
+/*eslint no-empty-function: ["error", { "allow": ["arrowFunctions"] }]*/
+/*eslint-env es6*/
+const noOp = () => {}; // used to indicate "no operation" for arg parsing
+
+yargs(hideBin(process.argv))
+    .strict()
+    .help('h')
+    .alias('h', 'help')
+    .command('get-stakepool-account', 'fetch the stake-pool account info', noOp, getStakePoolAccount)
+    .option('verbose', {
+      alias: 'v',
+      type: 'boolean',
+      description: 'Run with verbose logging'
+    })
+    .parse();
