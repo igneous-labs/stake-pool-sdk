523eb51e1a831aa315bb94877a962a0a0bcb4cb8
Jesse Y. Cho <jesse@socean.fi>
2022-02-05 16:09:43 -0800

    add referrerPoolTokensAccount arg

--- src/socean.ts
+++ src/socean.ts
@@ -3,7 +3,7 @@
  *
  * @module
  */
-import { Transaction } from "@solana/web3.js";
+import { Transaction, PublicKey } from "@solana/web3.js";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import BN from "bn.js"
 
@@ -23,34 +23,15 @@ export class Socean {
   /**
    * Returns Transaction that deposits sol into Socean stake pool
    */
-  async depositSol(amountLamports: BN, ): Promise<Transaction | null> {
+  async depositSol(amountLamports: BN, referrerPoolTokensAccount: PublicKey | null): Promise<Transaction | null> {
     const tx = new Transaction();
     console.log(amountLamports);
 
-    // check if the wallet public key as scnSOL associated token account
+    // TODO: check if the wallet public key as scnSOL associated token account
     // if not create one
     //tx.add();
 
     // prep deposit sol instruction
-    /**
-    * Initializes a DepositSol stake pool instruction given the required accounts and data
-    * @param stakePoolProgramId: Pubkey of the stake pool program
-    * @param stakePool: Pubkey of the stake pool to deposit to
-    * @param stakePoolWithdrawAuthority: Pubkey of the stake pool's withdraw authority.
-    *                                    PDA of the stake pool program, see StakePool docs for details.
-    * @param reserveStake: Pubkey of the stake pool's reserve account
-    * @param lamportsFrom: Pubkey of the SOL account to deduct SOL from to deposit.
-    * @param poolTokensTo: Pubkey of the pool token account to mint the pool tokens to.
-    * @param managerFeeAccount: Pubkey of the pool token account receiving the stake pool's fees.
-    * @param referrerPoolTokensAccount: Pubkey of the pool token account of the referrer to receive referral fees
-    * @param poolMint: Pubkey of the pool token mint
-    * @param tokenProgramId: Pubkey of the SPL token program
-    *
-    * @param amount: The amount of lamports to deposit
-    *
-    * @param solDepositAuthority: Optional Pubkey of the stake pool's deposit authority.
-    */
-
     const stakepool = await this.getStakePoolAccount();
     if (stakepool === null) return null;
     const ix = depositSolInstruction(
@@ -61,7 +42,7 @@ export class Socean {
       // TODO: lamportsFrom,
       // TODO: poolTokenTo,
       stakepool.account.data.managerFeeAccount,
-      // TODO: referrerPoolTokensAcount,
+      referrerPoolTokensAccount ?? stakepool.account.data.managerFeeAccount,
       stakepool.account.data.poolMint,
       TOKEN_PROGRAM_ID,
       amountLamports,
@@ -71,7 +52,6 @@ export class Socean {
     tx.add(ix);
     return tx;
   }
-
 
   /**
    * Retrieves and deserializes a StakePool account
